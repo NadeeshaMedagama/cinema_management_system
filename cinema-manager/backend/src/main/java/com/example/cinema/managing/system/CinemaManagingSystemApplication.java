b2888d848a21d1f93e977938d37a7e424b8f2fbe
Nadeesha Medagama <nadeeshamedagama@gmail.com>
2026-02-17 16:21:55 +0530

    backend main working files added

---
+++ cinema-manager/backend/src/main/java/com/example/cinema/managing/system/CinemaManagingSystemApplication.java
@@ -0,0 +1,12 @@
+package com.example.cinema.managing.system;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaManagingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemaManagingSystemApplication.class, args);
+	}
+
+}
